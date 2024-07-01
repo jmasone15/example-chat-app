034092a154e7a34a3d91d791f54ac788be37490e
Jordan <jordanmasone1@gmail.com>
2024-07-01 15:50:59 -0400

    Added sqlite for message caching

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import { error } from 'node:console';
+
+const db = await open({
+	filename: 'chat.db',
+	driver: sqlite3.Database
+});
+
+await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+`);
 
 const app = express();
 const server = createServer(app);
@@ -17,12 +33,36 @@ app.get('/', (req, res) => {
 	res.sendFile(join(__dirname, 'index.html'));
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
 	console.log('a user connected');
 
-	socket.on('chat message', (msg) => {
-		io.emit('chat message', msg);
+	socket.on('chat message', async (msg) => {
+		let result;
+
+		try {
+			result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+		} catch (e) {
+			console.error(error);
+			return;
+		}
+
+		io.emit('chat message', msg, result.lastID);
 	});
+
+	if (!socket.recovered) {
+		try {
+			await db.each(
+				'SELECT id, content FROM messages WHERE id > ?',
+				[socket.handshake.auth.serverOffset || 0],
+				(err, row) => {
+					socket.emit('chat message', row.content, row.id);
+				}
+			);
+		} catch (e) {
+			console.error(error);
+			return;
+		}
+	}
 
 	socket.on('disconnect', () => {
 		console.log('user disconnected');
